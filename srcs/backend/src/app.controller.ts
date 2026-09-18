61d74667b76be996d43c70d8ea569997cc7120f4
ybakun <ybakun@student.codam.nl>
2026-09-18 15:53:16 +0200

    add swagger docs

--- srcs/backend/src/app.controller.ts
+++ srcs/backend/src/app.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, HttpException, HttpStatus, Param } from '@nestjs/common';
 import type { ApiError } from '@appointment-saas/shared';
 import { AppService } from './app.service.js';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiTags('test-endpoint')
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @ApiTags('test-endpoint')
   @Get('health/:ok')
   health(@Param('ok') ok: string): { status: string } {
     if (ok !== 'ok') {
--- srcs/backend/src/main.ts
+++ srcs/backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // this setting should always be consistent with frontend/vite.config.ts
   app.setGlobalPrefix('api');
+
+  // swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('appointment-saas-api')
+    .setDescription('our project internal docs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
